2696e2359d7d7c3c820f5ead5b670ca8bc6e7f1e
Maria Shevchenko <shevtma@yandex.ru>
2022-11-12 02:00:49 +0300

    added script.js with function calculateBonus

---
+++ script.js
@@ -0,0 +1,13 @@
+const calculateBonus = (a, b) => {
+  let bonus;
+  const sum = a + b;
+  //debugger;
+  // выполняется подсчет суммы двух значений а и b, переданных при вызове функции
+  sum > 50 ? (bonus = 50) : (bonus = sum);
+  // если сумма больше 50, то бонусу присваивается значение 50, иначе бонусу присваивается значение sum
+  //debugger;
+  return bonus;
+};
+
+console.log(calculateBonus(30, 90));
+
